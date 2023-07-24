9047fe88d7a06682b0bf75619d98adb259a9ab40
ricklyon <rlyon14@gmail.com>
2023-07-23 23:00:24 -0600

    inital cli

--- pytex/preprocessor.py
+++ pytex/preprocessor.py
@@ -3,242 +3,298 @@ from time import time
 import sys
 import importlib
 import os
+import subprocess
+import shutil
+import click
 
-infile = Path(__file__).parent / 'test.tex' #Path(sys.argv[1])
-outfile = str(infile.parent) + '\\' + infile.stem + '_pp.tex'
-
-EMPTY = b''
-BACKSLASH =     '\\'.encode('utf-8')
-LEFT_BRACKET =  '['.encode('utf-8')
-RIGHT_BRACKET = ']'.encode('utf-8')
-LEFT_BRACE =    '{'.encode('utf-8')
-RIGHT_BRACE=    '}'.encode('utf-8')
-COMMENT=        '%'.encode('utf-8')
-NEWLINE=         '\n'.encode('utf-8')
-
-def parse_argument(stream):
-
-    argument = ''
-    nested_bracket = 0
-    arg_ch = ' '
-    key = None
-
-    while len(arg_ch):
-        if arg_ch == '{':
-            nested_bracket += 1
-        elif arg_ch == '}':
-            nested_bracket -= 1
-        elif (arg_ch == ',' or arg_ch == ']') and nested_bracket <=0:
-            stream.seek(-1, os.SEEK_CUR)
-            break
-
-        if arg_ch == '=':
-            key = argument.strip()
-            argument = ''
-
-        mname = get_macro_name(stream)
-        
-        if mname in imported_modules.keys():
-
-            output = get_macro_content(stream, mname)
-            argument += output
-
-        elif mname in defs:
-            argument += defs[mname]
-
-        elif mname:
-            argument += '\\' + mname
-
-        elif arg_ch != '}' and arg_ch != '{' and arg_ch != '=':
-            argument += arg_ch
-
-        
-        arg_ch = stream.read(1).decode('utf-8')
-
-    return key, argument.strip()
-
-def parse_macro_args(stream):
+class TeXPreprocessor(object):
+    """"
+    Searches a .tex file for all macro calls that are defined in Python and replaces it with the 
+    returned string of the Python method.
     """
-    Returns the argument list and kwargs passed into a pytex method. Stream
-    must be at the end of the pytex method name.
-    """
-    args = []
-    kwargs = {}
-    skip_whitespace(stream, allow_break=True)
+    EMPTY = ''
+    COMMENT= '%'
+    NEWLINE= '\n'
 
-    m_ch = stream.read(1).decode('utf-8')
+    def __init__(self):
+        self._in_stream  = None
+        self._out_stream = None
+        self._imported_modules = {}
+        self._defined_macros = {}
 
-    # return if no enclosing bracket around arguments
-    if m_ch != '[':
-        return args, kwargs
+    def advance(self, n:int=1):
+        """Returns the next n characters in the input stream and advances the current position."""
+        ch = self._in_stream.read(n).decode('utf-8')
+        return ch
     
-    # get macro arguments
-    key, arg = parse_argument(stream)
-    if key is None:
-        args.append(arg)
-    else:
-        kwargs[key] = arg
+    def peek(self, n:int=1):
+        """Returns the next n characters in the input stream without advancing the current position."""
+        n_ch = self.advance(n)
+        self.rewind(n)
+        return n_ch
 
-    m_ch = stream.read(1).decode('utf-8')
-    while m_ch == ',':
+    def rewind(self, n:int=1):
+        """Moves the current position of the input stream to the left by n characters."""
+        self._in_stream.seek(-n, os.SEEK_CUR)
 
-        key, arg = parse_argument(stream)
+    def write(self, data):
+        self._out_stream.write(data.encode())
+
+
+    def parse_argument(self):
+
+        argument = ''
+        nested_bracket = 0
+        arg_ch = ' '
+        key = None
+
+        while len(arg_ch):
+            if arg_ch == '{':
+                nested_bracket += 1
+            elif arg_ch == '}':
+                nested_bracket -= 1
+            elif (arg_ch == ',' or arg_ch == ']') and nested_bracket <=0:
+                self.rewind()
+                break
+
+            if arg_ch == '=':
+                key = argument.strip()
+                argument = ''
+
+            mname = self.get_macro_name()
+            
+            if mname in self._imported_modules.keys():
+
+                output = self.get_macro_content(mname)
+                argument += output
+
+            elif mname in self._defined_macros.keys():
+                argument += self._defined_macros[mname]
+
+            elif mname:
+                argument += '\\' + mname
+
+            elif arg_ch != '}' and arg_ch != '{' and arg_ch != '=':
+                argument += arg_ch
+
+            arg_ch = self.advance()
+
+        return key, argument.strip()
+
+    def parse_macro_args(self):
+        """
+        Returns the argument list and kwargs passed into a pytex method. Stream
+        must be at the end of the pytex method name.
+        """
+        args = []
+        kwargs = {}
+        self.skip_whitespace(allow_break=True)
+
+        m_ch = self.advance()
+
+        # return if no enclosing bracket around arguments
+        if m_ch != '[':
+            return args, kwargs
+        
+        # get macro arguments
+        key, arg = self.parse_argument()
         if key is None:
             args.append(arg)
         else:
             kwargs[key] = arg
 
-        m_ch = stream.read(1).decode('utf-8')
-    
-    if m_ch != ']':
-        raise ValueError('Unbalanced closing bracket around macro arguments.')
+        m_ch = self.advance()
+        while m_ch == ',':
 
-    return args, kwargs
+            key, arg = self.parse_argument()
+            if key is None:
+                args.append(arg)
+            else:
+                kwargs[key] = arg
+
+            m_ch = self.advance()
+        
+        if m_ch != ']':
+            raise ValueError('Unbalanced closing bracket around macro arguments.')
+
+        return args, kwargs
 
 
-def skip_whitespace(in_stream, out_stream=None, allow_break=False):
-    # read whitespace up to first non-whitespace character
+    def skip_whitespace(self, allow_break=False, trim:bool=False):
+        # read whitespace up to first non-whitespace character
 
-    ch = in_stream.read(1).decode('utf-8')  
+        ch = self.advance()
 
-    read_i = 1
-    while ch.isspace():
-        if ch == '\n' and not allow_break:
-            break
-        if out_stream is not None:
-            out_stream.write(ch.encode())
+        read_i = 1
+        while ch.isspace():
+            if ch == '\n' and not allow_break:
+                break
+            if not trim:
+                self.write(ch)
 
-        ch = in_stream.read(1).decode('utf-8')
-        read_i += 1
+            ch = self.advance()
+            read_i += 1
 
-    if len(ch):
-        in_stream.seek(-1, os.SEEK_CUR)
-    
-def get_macro_name(in_stream, out_stream=None):
-    """
-    Returns macro name. Stream must be at the first character of name (after backslash)
-    """
-    skip_whitespace(in_stream, out_stream)
-    n_ch = in_stream.read(1).decode('utf-8')
+        if len(ch):
+            self.rewind()
+        
+    def get_macro_name(self, trim_whitespace=False):
+        """
+        Returns macro name. Stream must be at the first character of name (after backslash)
+        """
+        self.skip_whitespace(trim=trim_whitespace)
+        n_ch = self.advance()
 
-    if n_ch != '\\':
+        if n_ch != '\\':
+            if len(n_ch):
+                self.rewind()
+            return None
+
+        name = ''
+        n_ch = self.advance()
+        while n_ch.isalpha():
+            name += n_ch
+            n_ch = self.advance()
+
         if len(n_ch):
-            in_stream.seek(-1, os.SEEK_CUR)
-        return None
+            self.rewind()
+        return name
 
-    name = ''
-    n_ch = in_stream.read(1).decode('utf-8')
-    while n_ch.isalpha():
-        name += n_ch
-        n_ch = in_stream.read(1).decode('utf-8')
+    def get_def_value(self):
+        """
+        Returns the value assigned to a pytex variable
+        """
+        value = ''
+        n_ch = self.advance()
 
-    if len(n_ch):
-        in_stream.seek(-1, os.SEEK_CUR)
-    return name
+        while n_ch != '\n' and len(n_ch):
+            value += n_ch
+            n_ch = self.advance()
 
-def get_def_value(stream):
-    """
-    Returns the value assigned to a pytex variable
-    """
-    value = ''
-    n_ch = stream.read(1).decode('utf-8')
+        if len(n_ch):
+            self.rewind()
+        return value.strip()
 
-    while n_ch != '\n' and len(n_ch):
-        value += n_ch
-        n_ch = stream.read(1).decode('utf-8')
+    def get_macro_content(self, mname):
+        module = self._imported_modules[mname]
 
-    if len(n_ch):
-        stream.seek(-1, os.SEEK_CUR)
-    return value.strip()
-
-def get_macro_content(stream, mname):
-    module = imported_modules[mname]
-
-    lib = importlib.__import__(module)
-
-    method_name = get_macro_name(stream)
-    if method_name is None:
-        raise ValueError('expected method name after imported module')
-    
-    method = getattr(lib, method_name)
-
-    # get macro arguments
-    args, kwargs = parse_macro_args(stream)
-
-    return method(*args, **kwargs)
-
-in_stream = f = open(infile, "rb")
-out_stream = open(outfile, "wb+")
-imported_modules = {}
-defs = {}
-
-stime = time()
-g_ch = b' '
-comment = False
-while len(g_ch):
-
-    if g_ch == COMMENT:
-        comment = True
-
-    if g_ch == NEWLINE:
-        comment = False
-
-    if comment:
-        if g_ch != COMMENT:
-            out_stream.write(g_ch)
-        g_ch = in_stream.read(1)
-        continue
-
-    mname = get_macro_name(in_stream, out_stream)
-
-    if mname is None:
-        g_ch = in_stream.read(1)
-        out_stream.write(g_ch)
+        method_name = self.get_macro_name(trim_whitespace=True)
+        if method_name is None:
+            raise ValueError('expected method name after imported module')
         
-    elif mname == 'import':
-        module = get_macro_name(in_stream)
-
-        if module is None:
-            raise ValueError('expected module name after import statement.')
-        
-        # check for alias
-        skip_whitespace(in_stream)
-        
-        alias_as = in_stream.read(2).decode('utf-8')
-        alias = module
-        if alias_as == 'as':
-            alias = get_macro_name(in_stream)
+        if module == 'pymacros':
+            lib = importlib.__import__('pytex')
+            module = getattr(lib, module)
+            method = getattr(module, method_name)
         else:
-            in_stream.seek(-2, os.SEEK_CUR)
-        imported_modules[alias] = module
+            lib = importlib.__import__(module)
+            method = getattr(lib, method_name)
 
-    elif mname == 'pydef':
-        varname = get_macro_name(in_stream)
+        # get macro arguments
+        args, kwargs = self.parse_macro_args()
 
-        if varname is None:
-            raise ValueError('expected variable name after pydef statement.')
-        
-        defs[varname] = get_def_value(in_stream)
+        return method(*args, **kwargs)
 
-    elif mname in imported_modules.keys():
+    def run(self, filepath):
+        infile = Path(filepath)
+        build_dir = Path(infile).parent / 'build'
 
-        output = get_macro_content(in_stream, mname)
-        out_stream.write(output.encode())
+        if not build_dir.is_dir():
+            os.mkdir(build_dir)
 
-    elif mname in defs:
-        out_stream.write(defs[mname].encode())
+        outfile = build_dir / (infile.stem + '.tex')
 
-    else:
-        out_stream.write('\\'.encode() + mname.encode())
+        self._in_stream  = open(infile, "rb")
+        self._out_stream = open(outfile, "wb+")
+        self._imported_modules = {}
+        self._defined_macros = {}
 
+        stime = time()
+        g_ch = b' '
+        comment = False
+        while len(g_ch):
 
+            if g_ch == self.COMMENT:
+                comment = True
 
+            if g_ch == self.NEWLINE:
+                comment = False
 
-print(time()-stime)
+            if comment:
+                if g_ch != self.COMMENT:
+                    self.write(g_ch)
+                g_ch = self.advance()
+                continue
 
-in_stream.close()
-out_stream.close()
+            mname = self.get_macro_name(trim_whitespace=False)
 
+            if mname is None:
+                g_ch = self.advance()
+                self.write(g_ch)
+                
+            elif mname == 'import':
+                module = self.get_macro_name(trim_whitespace=True)
 
+                if module is None:
+                    raise ValueError('expected module name after import statement.')
+                
+                # check for alias
+                self.skip_whitespace(trim=True)
+                
+                alias_as = self.advance(2)
+                alias = module
+                if alias_as == 'as':
+                    alias = self.get_macro_name(trim_whitespace=True)
+                else:
+                    self.rewind(2)
+                self._imported_modules[alias] = module
 
+            elif mname == 'pydef':
+                varname = self.get_macro_name(trim_whitespace=True)
 
+                if varname is None:
+                    raise ValueError('expected variable name after pydef statement.')
+                
+                self._defined_macros[varname] = self.get_def_value()
+
+            elif mname in self._imported_modules.keys():
+
+                output = self.get_macro_content(mname)
+                self.write(output)
+
+            elif mname in self._defined_macros:
+                self.write(self._imported_modules[mname])
+
+            else:
+                self.write('\\' + mname)
+
+        self._in_stream.close()
+        self._out_stream.close()
+
+        out = subprocess.run("pdflatex --synctex=1 --output-directory=\"{}\" {}".format(build_dir, outfile), check=True)
+
+        gen_pdf = build_dir / (infile.stem + '.pdf')
+        gen_syn = build_dir / (infile.stem + '.synctex.gz')
+
+        out_pdf = infile.with_suffix('.pdf')
+        out_syn = infile.with_suffix('.synctex.gz')
+
+        shutil.copyfile(gen_pdf, out_pdf)
+        shutil.copyfile(gen_syn, out_syn)
+
+        return out
+
+    # out = subprocess.run("\"C:/Program Files/SumatraPDF/SumatraPDF-3.4.6-64.exe\" \"{}\" -inverse-search \"\"C:/ProgramData/Notepad++/notepad++.exe\" -n%l \"%f\"\"".format(out_pdf), shell=True)
+
+@click.command()
+@click.argument('filepath')
+def cli(filepath):
+    texpp = TeXPreprocessor()
+    texpp.run(filepath)
+
+if __name__ == '__main__':
+    
+    # cli()
+    texpp = TeXPreprocessor()
+    texpp.run(Path(__file__).parent / 'test.tex')
+    
